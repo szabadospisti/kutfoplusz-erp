b5b170ffc19ea701eae3ccf43d2d611eea7a7522
szabadospisti <szabadospisti@icloud.com>
2026-08-20 12:37:42 +0200

    Remove obsolete Géppark module

--- fleet-machine-profile.js
+++
@@ -1,9 +0,0 @@
-/* Kútfő Plusz ERP – Géppark régi adatlap kompatibilitási réteg
- * A korábbi modalos gépadlap-modult letiltottuk.
- * A jelenlegi Géppark adatlapot a machine-profile-native.js kezeli,
- * a szerkesztést pedig a machine-profile-edit-inline.js.
- */
-(function(){
-  'use strict';
-  window.__legacyFleetMachineProfileDisabled=true;
-})();
