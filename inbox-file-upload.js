4bc3690dd0e503112006d64e816fb40e8d41816e
szabadospisti <szabadospisti@icloud.com>
2026-08-27 17:01:06 +0200

    Remove uploaded ERP files

--- inbox-file-upload.js
+++
@@ -1,4 +0,0 @@
-/* Beérkező dokumentum fájlfeltöltés – ideiglenesen kikapcsolva.
-   A fájlfeltöltő MutationObserver / submit-hook nem futhat az ERP alap
-   működése mellett, amíg az inbox modult újra nem építjük stabilan.
-*/
